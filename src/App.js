7215f7468072aaa2cc99ca6bbff52dd7f63278b3
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2025-12-12 23:29:40 +0200

    done

--- src/App.js
+++ src/App.js
@@ -22,16 +22,6 @@ function App() {
   const handleLoadMore = () => {
     setPage((prevPage) => prevPage + 1);
   };
-
-
-  // function displayMovieDetails(movieId) {
-  //   const URL = `https://api.themoviedb.org/3/movie/?api_key=${API_KEY}&language=en-US`
-  //   fetch(URL)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //     });
-  // }
   return (
     <div className="App">
       <Navbar />
@@ -43,15 +33,13 @@ function App() {
         <div className="row g-4">
           {movies.map((movie) => (
             <Card
-              // image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              // key={movie.id}
               movie={movie}
             />
           ))}
         </div>
       </div>
       <div className="d-flex justify-content-center p-4">
-        <button className="btn btn-success " onClick={handleLoadMore}>
+        <button className="btn btn-success w-25 h-25" onClick={handleLoadMore}>
           <i className="bi bi-cloud-upload-fill"></i> Load More
         </button>
       </div>
