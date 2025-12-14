d59f71f9aa32e8485e75140dcd76a091f27a418f
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2025-12-14 16:36:03 +0200

    add search api and test it if it work or not

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import MovieDetails from './pages/MovieDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<App />} />
@@ -19,8 +18,6 @@ root.render(
         <Route path="/movie/:id" element={<MovieDetails />} />
       </Routes>
     </BrowserRouter>
-
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
